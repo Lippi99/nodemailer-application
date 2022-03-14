a88a546dfe4801264a8b4738723384c068358b1b
Felipe Salomao <lipesalomaoclaf@gmail.com>
2022-03-13 22:08:48 -0300

    added app use

--- index.ts
+++ index.ts
@@ -1,37 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import nodemailer from "nodemailer";
+import emailRoute from "./Route/email";
 
 dotenv.config();
 
 const app = express();
 
-const sendEmail = async (sendTo: string) => {
-  const transporterConfig = {
-    host: "smtp-mail.outlook.com",
-    port: 587,
-    auth: {
-      user: process.env.email,
-      pass: process.env.password,
-    },
-  };
+app.use(express.json());
+app.use("/email", emailRoute);
 
-  const transporter = nodemailer.createTransport(transporterConfig);
-
-  const email = {
-    from: process.env.email,
-    to: sendTo,
-    subject: "test",
-    text: "This is a email test",
-    html: "<b>Here you can send your own html message</b>",
-  };
-
-  transporter.sendMail(email, (err, result) => {
-    if (err) return console.log(err);
-    console.log("Mensagem enviada! " + result.response);
-  });
-};
-
-//requires one argument (loremipsum@hotmail.com)
-sendEmail("loremipsum@hotmail.com");
 app.listen(process.env.api, () => console.log("Application started"));
